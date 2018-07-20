21b35226767307c2c005c6be28f3e1d0bc4a75e6
Nicollas <n.matheuscarvalho@gmail.com>
2018-07-19 23:54:20 -0300

    logic finished

--- index.ios.js
+++ index.ios.js
@@ -28,12 +28,41 @@ class App extends Component {
     optComputer[0]= "Pedra"
     optComputer[1]= "Tesoura"
     optComputer[2]= "Papel"
-    var sorteio = Math.random()*3
-    sorteio = Math.floor(sorteio)
+    var sorteio = Math.floor(Math.random()*3)
+    var res
+
+    if(escolhaUser === 'Pedra'){
+      if(optComputer[sorteio] === 'Pedra'){
+        res = 'Empate'
+      }else if(optComputer[sorteio] === 'Papel'){
+        res = 'Perdeu'
+      }else if(optComputer[sorteio] === 'Tesoura'){
+        res = 'Ganhou'
+      }
+    }
+    if(escolhaUser === 'Papel'){
+      if(optComputer[sorteio] === 'Pedra'){
+        res = 'Ganhou'
+      }else if(optComputer[sorteio] === 'Papel'){
+        res = 'Empate'
+      }else if(optComputer[sorteio] === 'Tesoura'){
+        res = 'Perdeu'
+      }
+    }
+    if(escolhaUser === 'Tesoura'){
+      if(optComputer[sorteio] === 'Pedra'){
+        res = 'Perdeu'
+      }else if(optComputer[sorteio] === 'Papel'){
+        res = 'Ganhou'
+      }else if(optComputer[sorteio] === 'Tesoura'){
+        res = 'Empate'
+      }
+    }
 
     this.setState({
       escolhaUsuario: escolhaUser,
       escolhaComputador: optComputer[sorteio],
+      resultado: res
     })
 
     
@@ -44,7 +73,7 @@ class App extends Component {
       <View style={Estilo.geral}>
         <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
         <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-        <Text>Resultado: </Text>
+        <Text>Resultado: {this.state.resultado} </Text>
         <Button title="Pedra" onPress={() => this.mudaEstado("Pedra")}></Button>
         <Button title="Papel" onPress={() => this.mudaEstado("Papel")}></Button>
         <Button title="Tesoura" onPress={() => this.mudaEstado("Tesoura")}></Button>
